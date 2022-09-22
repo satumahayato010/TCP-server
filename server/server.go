e56f3b005bcf853bd4a85c420a0d0ac444e59165
stm33 <satuma33@icloud.com>
2022-09-22 18:38:50 +0900

    [add]refs #3 listener-create

--- server/server.go
+++ server/server.go
@@ -1,1 +1,29 @@
-package server
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func receiveTCPConn(ln *net.TCPListener) {
+	for {
+		conn, err := ln.AcceptTCP()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(conn)
+	}
+}
+
+func main() {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ln, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	receiveTCPConn(ln)
+}
